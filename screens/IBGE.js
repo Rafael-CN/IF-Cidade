6d0fbdbc599c1c092b1486df273238927557fb62
Rafael <rcn.ytay+work@gmail.com>
2024-07-03 00:54:14 -0300

    IBGE screen

--- screens/IBGE.js
+++ screens/IBGE.js
@@ -1,17 +1,91 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Route } from "../common/Screen";
 import { Theme } from "../common/Theme";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+function IBGEInfo({
+	title,
+	icon,
+	value,
+	theme = "primary",
+	alignSelf = "flex-start",
+}) {
+	const colors = {
+		primary: {
+			main: Theme.primary,
+			light: Theme.primaryLight,
+		},
+		secondary: {
+			main: Theme.secondary,
+			light: Theme.secondaryLight,
+		},
+	};
+
+	const infoStyle = StyleSheet.create({
+		info: {
+			backgroundColor: colors[theme].light,
+			paddingHorizontal: 20,
+			paddingVertical: 15,
+			borderRadius: 20,
+			alignSelf: alignSelf,
+		},
+
+		infoTitle: {
+			color: colors[theme].main,
+			fontSize: 18,
+		},
+
+		infoContent: {
+			flexDirection: "row",
+			alignItems: "center",
+			marginTop: 5,
+			gap: 20,
+		},
+
+		infoText: {
+			color: colors[theme].main,
+			fontSize: 25,
+			fontWeight: "700",
+		},
+
+		infoIcon: {
+			color: colors[theme].main,
+			fontSize: 25,
+		},
+	});
+
+	return (
+		<View style={infoStyle.info}>
+			<Text style={infoStyle.infoTitle}>{title}</Text>
+			<View style={infoStyle.infoContent}>
+				<Icon style={infoStyle.infoIcon} name={icon}></Icon>
+				<Text style={infoStyle.infoText}>{value}</Text>
+			</View>
+		</View>
+	);
+}
 
 function IBGE() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<IBGEInfo title="Área territorial" icon="map" value="4.090 km²" />
+			<IBGEInfo
+				theme="secondary"
+				title="População"
+				icon="account-group"
+				value="117.938"
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: Theme.background,
-		alignItems: "center",
-		justifyContent: "center",
+		paddingHorizontal: 30,
+		paddingTop: 60,
+		gap: 20,
 	},
 });
 
