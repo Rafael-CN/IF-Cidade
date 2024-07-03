137c5566cfb4e2e7369970e146d1caa01e8e943a
calb-seppherus <gabriel.freitas2006@proton.me>
2024-07-03 15:57:25 -0300

    tourism screen

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { Screens } from "./screens";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import { Theme } from "./common/Theme";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import * as NavigationBar from "expo-navigation-bar";
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-	return (
-		<NavigationContainer>
-			<Tab.Navigator
-				activeColor={Theme.dark}
-				inactiveColor={Theme.dark}
-				barStyle={{ backgroundColor: Theme.primaryLight }}
-				activeIndicatorStyle={{ backgroundColor: "rgba(0,50,0,0.2)" }}
-			>
-				{Screens.map((s, i) => {
-					return (
-						<Tab.Screen
-							key={i}
-							name={s.screen.name}
-							component={s.screen}
-							options={{
-								tabBarIcon: ({ color }) => (
-									<Icon name={s.icon} color={color} size={26}></Icon>
-								),
-							}}
-						></Tab.Screen>
-					);
-				})}
-			</Tab.Navigator>
-		</NavigationContainer>
-	);
+  NavigationBar.setBackgroundColorAsync(Theme.primaryLight);
+  NavigationBar.setButtonStyleAsync("dark");
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        activeColor={Theme.dark}
+        inactiveColor={Theme.dark}
+        barStyle={{ backgroundColor: Theme.primaryLight }}
+        activeIndicatorStyle={{ backgroundColor: "rgba(0,50,0,0.2)" }}
+      >
+        {Screens.map((s, i) => {
+          return (
+            <Tab.Screen
+              key={i}
+              name={s.screen.name}
+              component={s.screen}
+              options={{
+                tabBarIcon: ({ color }) => (
+                  <Icon name={s.icon} color={color} size={26}></Icon>
+                ),
+              }}
+            ></Tab.Screen>
+          );
+        })}
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/IBGE.js
+++ screens/IBGE.js
@@ -4,89 +4,111 @@ import { Theme } from "../common/Theme";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 function IBGEInfo({
-	title,
-	icon,
-	value,
-	theme = "primary",
-	alignSelf = "flex-start",
+  title,
+  icon,
+  value,
+  theme = "primary",
+  alignSelf = "flex-start",
 }) {
-	const colors = {
-		primary: {
-			main: Theme.primary,
-			light: Theme.primaryLight,
-		},
-		secondary: {
-			main: Theme.secondary,
-			light: Theme.secondaryLight,
-		},
-	};
+  const colors = {
+    primary: {
+      main: Theme.primary,
+      light: Theme.primaryLight,
+    },
+    secondary: {
+      main: Theme.secondary,
+      light: Theme.secondaryLight,
+    },
+  };
 
-	const infoStyle = StyleSheet.create({
-		info: {
-			backgroundColor: colors[theme].light,
-			paddingHorizontal: 20,
-			paddingVertical: 15,
-			borderRadius: 20,
-			alignSelf: alignSelf,
-		},
+  const fontSize = 25;
 
-		infoTitle: {
-			color: colors[theme].main,
-			fontSize: 18,
-		},
+  const infoStyle = StyleSheet.create({
+    info: {
+      backgroundColor: colors[theme].light,
+      paddingHorizontal: 20,
+      paddingVertical: 15,
+      borderRadius: 15,
+      alignSelf: alignSelf,
+    },
 
-		infoContent: {
-			flexDirection: "row",
-			alignItems: "center",
-			marginTop: 5,
-			gap: 20,
-		},
+    infoTitle: {
+      color: colors[theme].main,
+      fontSize: fontSize * 0.7,
+    },
 
-		infoText: {
-			color: colors[theme].main,
-			fontSize: 25,
-			fontWeight: "700",
-		},
+    infoContent: {
+      flexDirection: "row",
+      alignItems: "center",
+      marginTop: 5,
+      gap: 20,
+    },
 
-		infoIcon: {
-			color: colors[theme].main,
-			fontSize: 25,
-		},
-	});
+    infoText: {
+      color: colors[theme].main,
+      fontSize: fontSize,
+      fontWeight: "700",
+    },
 
-	return (
-		<View style={infoStyle.info}>
-			<Text style={infoStyle.infoTitle}>{title}</Text>
-			<View style={infoStyle.infoContent}>
-				<Icon style={infoStyle.infoIcon} name={icon}></Icon>
-				<Text style={infoStyle.infoText}>{value}</Text>
-			</View>
-		</View>
-	);
+    infoIcon: {
+      color: colors[theme].main,
+      fontSize: fontSize,
+    },
+  });
+
+  return (
+    <View style={infoStyle.info}>
+      <Text style={infoStyle.infoTitle}>{title}</Text>
+      <View style={infoStyle.infoContent}>
+        <Icon style={infoStyle.infoIcon} name={icon}></Icon>
+        <Text style={infoStyle.infoText}>{value}</Text>
+      </View>
+    </View>
+  );
 }
 
 function IBGE() {
-	return (
-		<View style={styles.container}>
-			<IBGEInfo title="Área territorial" icon="map" value="4.090 km²" />
-			<IBGEInfo
-				theme="secondary"
-				title="População"
-				icon="account-group"
-				value="117.938"
-			/>
-		</View>
-	);
+  const data = [
+    { icon: "map", title: "Área territorial", value: "4.090 km²" },
+    { icon: "account-group", title: "População", value: "117.938" },
+    { icon: "school", title: "Escolarização", value: "97,6%" },
+    {
+      icon: "map-marker-radius",
+      title: "Densidade demográfica",
+      value: "28,83 hab/km²",
+    },
+    { icon: "chart-line-variant", title: "IDHM", value: "0,740" },
+    { icon: "cash", title: "PIB per capita", value: "R$ 30.846" },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {data.map((d, i) => {
+        return (
+          <IBGEInfo
+            key={i}
+            theme={i % 2 === 0 ? "primary" : "secondary"}
+            title={d.title}
+            icon={d.icon}
+            value={d.value}
+          ></IBGEInfo>
+        );
+      })}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: Theme.background,
-		paddingHorizontal: 30,
-		paddingTop: 60,
-		gap: 20,
-	},
+  container: {
+    flex: 1,
+    backgroundColor: Theme.background,
+    paddingHorizontal: 30,
+    paddingTop: 60,
+    rowGap: 20,
+    columnGap: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
 });
 
 export default new Route(IBGE, "chart-donut");
